54da54acfc6f1d6ea427b179bda3f8d887ec9931
GCHQDeveloper500 <GCHQDeveloper500@github.noreply.example.com>
2019-02-11 16:20:53 +0000

    Security Updates

--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,7 @@ module.exports = {
         use: [{ 
           loader: 'babel-loader' ,
           query: {
-            presets: ['es2015'],
-            plugins: ['transform-object-assign']
+            presets: ['env']
           }
         }],
         
